324336cc7777e34d21bfc57d6507d1680d8d4c3a
Xudong Liu <xudong.camsys@gmail.com>
2018-03-06 16:18:02 -0500

    fix types

--- src/models/itinerary.ts
+++ src/models/itinerary.ts
@@ -1,5 +1,6 @@
 import { Customer } from '../models/customer';
 
 export class Itinerary {
+  id: number;
   customer: Customer;
 }
--- src/providers/ridepilot/ridepilot.ts
+++ src/providers/ridepilot/ridepilot.ts
@@ -34,11 +34,10 @@ export class RidepilotProvider {
 
   // Get list of today's run
   getRuns(): Observable<Run[]> {
-
     return this.http
                .get(this.baseAvlUrl + 'runs', this.requestOptions())
                .map( response => this.unpackRunsResponse(response))
-               .catch(error => this.handleError(error));
+               .catch((error: Response) =>  this.handleError(error));
   }
 
   // Get run manifest
@@ -50,19 +49,18 @@ export class RidepilotProvider {
     return this.http
                .get(uri, this.requestOptions())
                .map( response => this.unpackItinerariesResponse(response))
-               .catch(error => this.handleError(error));
+               .catch((error: Response) =>  this.handleError(error));
   }
 
   // Parse Runs response
-  private unpackRunsResponse(response: any): Run[] {
-    let runs: any = response.json().data || [];
+  private unpackRunsResponse(response): Run[] {
+    let runs = response.json().data || [];
     let runModels: Run[] = runs.map(run => this.parseRun(run));
-    console.log(runModels);
     return  runModels;
   }
 
   // Parse individual run
-  private parseRun(response: {}): Run {
+  private parseRun(response): Run {
     let run: Run = response.attributes as Run;
     run.id = response.id;
 
@@ -70,24 +68,23 @@ export class RidepilotProvider {
   }
 
   // Parse itineraries response
-  private unpackItinerariesResponse(response: any): Run[] {
-    let itineraries: any = response.json().data || [];
+  private unpackItinerariesResponse(response): Itinerary[] {
+    let itineraries = response.json().data || [];
     let itinModels: Itinerary[] = itineraries.map(itin => this.parseItinerary(itin));
     return  itinModels;
   }
 
   // Parse individual itinerary
-  private parseItinerary(response: {}): Run {
+  private parseItinerary(response): Itinerary {
     let itin: Itinerary = response.attributes as Itinerary;
     itin.id = response.id;
 
     return itin;
   }
 
-
   // Handle errors by console logging the error, and publishing an error event
   // for consumption by the app's home page.
-  private handleError(error: any): any {
+  private handleError(error: Response | any): Observable<any> {
     console.error('An error occurred', error, this); // for demo purposes only
     this.events.publish('error:http', error);
     return Observable.empty(); // return an empty observable so subscribe calls don't break
